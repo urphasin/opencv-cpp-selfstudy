2b84b14e48c7a37ace370d819781701a280deaea
otito <otitoamuga@gmail.com>
2025-06-07 16:17:37 -0400

    other external camera

--- 01-basics/capture_webcam.cpp
+++ 01-basics/capture_webcam.cpp
@@ -2,8 +2,18 @@
 #include <iostream>
 
 int main() {
-  // Open the default camera (0)
-  cv::VideoCapture cap(0);
+  std::cout << "Checking for available video capture devices...\n";
+
+  // for (int i = 0; i < 10; ++i) { // Check first 10 devices indices
+  //   cv::VideoCapture cap(i);
+  //   if (cap.isOpened() == true) {
+  //     std::cout << "\033[33mDevice found at index: " << i << "\033[0m" << std::endl;
+  //     cap.release(); // Always release!
+  //   }
+  // }
+
+    // Open the default camera (0)
+  cv::VideoCapture cap(2);
   if (cap.isOpened() == false) {
     std::cerr << "Error: Cannot open the webcam" << std::endl;
     return -1;
